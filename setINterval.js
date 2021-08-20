75e7bf123699b33358609e8428e7bbc2bebea744
foziaShareen <foziashareen1@gmail.com>
2021-08-20 12:07:23 +0500

    set interval added

---
+++ setINterval.js
@@ -0,0 +1,12 @@
+function OurReact(){
+    return React.createElement("div",null,[
+      React.createElement("h1",null,"it is done"),
+      React.createElement("h1",null,`current time is ${new Date().toLocaleString()}`),
+    ])
+  
+  }
+  setInterval(function(){
+   ReactDOM.render(React.createElement(OurReact),document.querySelector("#app")) 
+  },1000)
+  //in html 
+  // <div id="app"></div>
